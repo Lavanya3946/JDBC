d3d60fadca61b4a0f67d8244545667575477c38e
Lavanya121003 <lavanyaanand1210@gmail.com>
2024-12-23 19:42:01 +0530

    jdbc steps

---
+++ jdbc-app/src/main/java/com/xowrkz/jdbcapp/runner/LaptopRunner.java
@@ -0,0 +1,23 @@
+package com.xowrkz.jdbcapp.runner;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LaptopRunner {
+
+    public static void main(String[] args) {
+        System.out.println("running the main method..");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("connection successful");
+            String url = "jdbc:mysql://localhost:3306/super_market";
+            String userName = "root";
+            String password = "root";
+            DriverManager.getConnection(url, userName, password);
+            System.out.println("Jdbc connection done");
+        } catch (ClassNotFoundException | SQLException classNotFoundException) {
+            System.out.println("JDBC class not found:" + classNotFoundException.getMessage());
+        }
+    }
+}
+
---
+++ jdbc-app/src/main/java/com/xowrkz/jdbcapp/runner/TabRunner.java
@@ -0,0 +1,22 @@
+package com.xowrkz.jdbcapp.runner;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TabRunner {
+    public static void main(String[] args) {
+        System.out.println("running the main method..");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("connection successful");
+            String url = "jdbc:mysql://localhost:3306/super_market";
+            String userName = "root";
+            String password = "root";
+            DriverManager.getConnection(url, userName, password);
+            System.out.println("Jdbc connection done");
+        } catch (ClassNotFoundException | SQLException classNotFoundException) {
+            System.out.println("JDBC class not found:" + classNotFoundException.getMessage());
+        }
+    }
+}
+
