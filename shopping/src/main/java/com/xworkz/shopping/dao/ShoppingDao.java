bf8c39ecb1c3a329d479103a9195866a45817c40
Lavanya A <lavanyaanand1210@gmail.com>
2025-01-01 23:20:44 +0530

    autocommit

---
+++ shopping/src/main/java/com/xworkz/shopping/dao/ShoppingDao.java
@@ -0,0 +1,10 @@
+package com.xworkz.shopping.dao;
+
+import java.sql.ResultSet;
+
+public interface ShoppingDao {
+    int save(int id,String shop_name,String shopKeeper_name,String shop_location,String purchased_item,int item_cost);
+    ResultSet getDataById(int id);
+    int UpdatebyField(int id,String shop_name,String shopKeeper_name,String shop_location,String purchased_item,int item_cost);
+
+}
